d5f574e57986f5becb3122fb38766caf9d403dea
zavod <gdevil558@gmail.com>
2024-12-26 21:33:41 +0200

    Update googleOAuth2.js

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -7,12 +7,12 @@ import { env } from './env.js';
 
 const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
 
-const oauthConfig = JSON.parse(await readFile(PATH_JSON));
+// const oauthConfig = JSON.parse(await readFile(PATH_JSON));
 
 const googleOAuthClient = new OAuth2Client({
   clientId: env('GOOGLE_AUTH_CLIENT_ID'),
   clientSecret: env('GOOGLE_AUTH_CLIENT_SECRET'),
-  redirectUri: oauthConfig.web.redirect_uris[0],
+  // redirectUri: oauthConfig.web.redirect_uris[0],
   // redirectUri: env('GOOGLE_REDIRECT_URL')
 });
 
