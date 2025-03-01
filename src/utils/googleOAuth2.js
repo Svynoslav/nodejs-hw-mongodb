6922b7398ada2539bb3e1db3398ecaaf30189327
svynoslav <badbloodprince@gmail.com>
2025-03-01 18:37:35 +0200

    Update googleOAuth2.js

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -5,9 +5,9 @@ import { OAuth2Client } from 'google-auth-library';
 import { env } from './env.js';
 
 const googleOAuth2Client = new OAuth2Client({
-  clientId: env('GOOGLE_OAUTH_CLIENT_ID'),
-  clientSecret: env('GOOGLE_OAUTH_CLIENT_SECRET'),
-  redirectUri: env('GOOGLE_OAUTH_REDIRECT_URL'),
+  clientId: env('GOOGLE_AUTH_CLIENT_ID'),
+  clientSecret: env('GOOGLE_AUTH_CLIENT_SECRET'),
+  redirectUri: env('GOOGLE_AUTH_REDIRECT_URL'),
 });
 
 export function generateOAuthURL() {
